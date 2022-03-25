7a75704d1baf387d47f9c5b10ed5c4c7cac89bae
Alex Godfrey <alexjamesgodfrey@gmail.com>
2022-03-25 15:12:28 -0400

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Layout } from 'antd';
+import LeftSideBar from 'components/LeftSideBar';
+import ClassSearch from 'components/ClassSearch';
+
+function App() {
+  return (
+    <Layout className="full-height">
+      <Layout>
+        <LeftSideBar />
+        <Layout.Content>
+          <ClassSearch />
+        </Layout.Content>
+      </Layout>
+    </Layout>
+  );
+}
+
+export default App;
